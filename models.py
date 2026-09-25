3790caff504f5ae8f829d5745c5c05ee8ccb8d0e
Prithviraj-Singh0268 <officialprithviraj268@gmail.com>
2026-09-25 14:32:51 +0530

    Add Task model with SQLAlchemy and create MySQL table and changing requirment.txt

---
+++ models.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.String(400), nullable=True)
+    priority = db.Column(db.String(10), default="Medium")
+    completed = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Task {self.id}: {self.title}>"
